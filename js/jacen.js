a1eed55ca5f1fda337adebc08e1fd61232050317
unknown <NGeorgiou@NGEORGIOU.LFHQ.lfp.com>
2014-07-17 15:44:20 -0700

    Heres all the stuff from my windows machine as a test. It will all be erased though

---
+++ js/jacen.js
@@ -0,0 +1,5 @@
+function jacen(){
+	console.log("Although it wouldnt be too shabby to have Jacen's powers either. ");
+	console.log('/usr/local/lib/node_modules/npm');
+}
+jacen();
---
+++ js/luke.js
@@ -0,0 +1,4 @@
+function luke(){
+	console.log("damn it i wish I had luke's powers and mastery of the Force");
+}
+luke();
